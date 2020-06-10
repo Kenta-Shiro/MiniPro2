a27f9fdf1c9aa9f4eb122ed534491ed344ca00c5
Han Seunghee <p190953@cresco.co.jp>
2020-06-10 15:51:11 +0900

    修正

--- 実装/TitleCon.java
+++ 実装/TitleCon.java
@@ -1,17 +1,27 @@
-package 設計;
+package minipro2;
+
+import java.util.List;
 
 public class TitleCon {
 
 	private TitleList titleList;
 
-	private TitleList titleList;
-
-	public TitleList addTitle(String title) {
-		return null;
-	}
-
 	public TitleCon(TitleList titleList) {
-
+		this.titleList = titleList;
+	}
+	
+	public TitleList addTitle(String title) {
+		List<Title> tempList = titleList.getTitleList();
+		//判定
+		for(Title temp:tempList) {
+			if(title.equals(temp.getTitle())) {
+				System.out.println("タイトルが重複しています");
+			}else {
+				titleList.makeTitle(title);
+				System.out.println("タイトルが追加されました");
+			}
+		}
+		return titleList;
 	}
 
 }
