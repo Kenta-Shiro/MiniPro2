77adba289ca29a9d09c475c1e32f1d49be03e65a
Yuma-c52 <y-nakane@cresco.co.jp>
2020-06-11 11:38:09 +0900

    あ

--- テスト/ReviewListTest.java
+++ テスト/ReviewListTest.java
@@ -3,33 +3,34 @@ package minipro2;
 
 import java.util.List;
 
-public class ReviewListTest {
+public class Test {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		ReviewList r = new ReviewList("ワンピース",1);
 		Member m = new Member("鈴木太郎","abcd","2021");
-		r.makeReview("いい", m);
+		r.makeReview("ワンピース","いい", m);
 		
 		System.out.println("getReviewListテスト");
 		List<Review> l = r.getReviewList();
 		
 		for(Review temp:l) {
-			System.out.println(temp.getReviewId()+"."+temp.getContent());
+			System.out.println(temp.getReviewNo()+"."+temp.getContent());
 		}
 		
 		System.out.println("makeReviewテスト");
-		r.makeReview("嫌い", m);
+		r.makeReview("ワンピース","嫌い", m);
 		
 		for(Review temp:l) {
-			System.out.println(temp.getReviewId()+"."+temp.getContent());
+			System.out.println(temp.getReviewNo()+"."+temp.getContent());
 		}
 		
 		System.out.println("deleteReviewテスト");
-		r.deleteReview(3);
+		
+		r.deleteReview(1);
 		
 		for(Review temp:l) {
-			System.out.println(temp.getReviewId()+"."+temp.getContent());
+			System.out.println(temp.getReviewNo()+"."+temp.getContent());
 		}
 
 	}
