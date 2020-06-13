752ce5361ea011db8e0f820c32917bf1dddcdeb2
t-takei104 <t-takei@cresco.co.jp>
2020-06-13 13:54:11 +0900

    minipro3にDAOフォルダを移動

--- 実装/第3反復/review.sql
+++
@@ -1,9 +0,0 @@
-create table review(
-reviewno int not null PRIMARY KEY,
-titleno int not null,
-member_id varchar(10) not null,
-reviewdate datetime not null,
-content varchar(100),
-FOREIGN KEY (titleno) REFERENCES title(titleno),
-FOREIGN KEY (member_id) REFERENCES member(id),
-)
